d3583deb0fd571edbf454813b92f66a4c851e3e4
richard <theirsaviorhd@gmail.com>
2023-01-14 15:11:04 -0500

    Random Prompts
    a random number generator has been implimented, will be used for random prompts
    fixed content overflow on the header section for smaller devices

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const dateContainer = document.querySelector('.date')
 const content = document.createElement('h2')
 content.textContent = Date();
 dateContainer.appendChild(content);
+
+//generate random number for prompts
+const randomNumber = Math.floor(Math.random()* 78);
+console.log(randomNumber);
